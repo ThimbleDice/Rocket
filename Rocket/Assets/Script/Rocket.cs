9b72a0a01ad92a095f8363dc60b5ae0d2ebc9195
B533P-07\533 <thimbledice@gmail.com>
2018-08-24 16:28:45 -0400

    Création et application du script Rocket

---
+++ Rocket/Assets/Script/Rocket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    public Rigidbody rocketRigidBody;
+    [SerializeField] public float boostPower = 100;
+
+    // Use this for initialization
+    void Start () {
+        rocketRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RocketBoost();
+    }
+
+    private void RocketBoost()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            rocketRigidBody.AddForce(new Vector3(0, boostPower, 0));
+        }
+    }
+}
